7b9d834f8040c44f054de8f7faa73d393f676035
NorwayFun <72336380+NorwayFun@users.noreply.github.com>
2024-10-11 06:45:26 +0200

    Add Georgian

---
+++ iTestClient/i18n/iTestClient-ka.ts
@@ -0,0 +1,416 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka_GE">
+<context>
+    <name>AboutWidget</name>
+    <message>
+        <source>About iTest</source>
+        <translation>iTest-ის შესახებ</translation>
+    </message>
+    <message>
+        <source>About Qt</source>
+        <translation>Qt-ის შესახებ</translation>
+    </message>
+    <message>
+        <source>This program is distributed under the terms of the GPL v2.</source>
+        <translation>პროგრამა ვრცელდება ლიცენზიით GPL v2.</translation>
+    </message>
+    <message>
+        <source>The program is provided AS IS with ABSOLUTELY NO WARRANTY OF ANY KIND, INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.</source>
+        <translation>პროგრამა მოწოდებულია როგორც არის, ყოველგვარი გარანტიის გარეშე, მათ შორის, დიზაინის, სავაჭრო და კონკრეტული მიზნისთვის გამოსადეგობისთვის.</translation>
+    </message>
+    <message>
+        <source>iTest</source>
+        <translation>iTest</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>ვერსია %1</translation>
+    </message>
+    <message>
+        <source>Licence</source>
+        <translation>ლიცენზია</translation>
+    </message>
+    <message>
+        <source>iTest is an application suite consisting of a Server and a Client designed for easy computerised examination.</source>
+        <translation>iTest აპლიკაციაა, რომელიც სერვერისა და კლიენტისგან შედგება და განკუთვნილია კომპიუტერიზებული გამოცდებისთვის.</translation>
+    </message>
+    <message>
+        <source>http://www.gnu.org/licenses/gpl-2.0.html</source>
+        <translation>http://www.gnu.org/licenses/gpl-2.0.html</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Retrieving test data...</source>
+        <translation>ტესტის მონაცემების მიღება...</translation>
+    </message>
+    <message>
+        <source>Save answer log</source>
+        <translation>პასუხების ჟურნალის შენახვა</translation>
+    </message>
+    <message>
+        <source>Cannot write file %1:
+%2.</source>
+        <translation>ფაილის %1 ჩაწერის შეცდომა:
+%2.</translation>
+    </message>
+    <message>
+        <source>The host was not found. Please check the host name and port settings.</source>
+        <translation>ჰოსტი აღმოჩენილი არაა. შეამოწმეთ ჰოსტის სახელი და პორტის პარამეტრები.</translation>
+    </message>
+    <message>
+        <source>The connection was refused by the peer. Make sure the iTest server is running, and check that the host name and port settings are correct.</source>
+        <translation>კავშირი უარყოფილი იყო პარტნიორის მიერ. დარწმუნდით, რომ iTest-ის სერვერი გაშვებულია და შეამოწმეთ, სწორია თუ არა ჰოსტის სახელისა და პორტის პარამეტრები.</translation>
+    </message>
+    <message>
+        <source>The following error occurred: %1.</source>
+        <translation>აღმოჩენილია შემდეგი შეცდომა: %1.</translation>
+    </message>
+    <message>
+        <source>Open database file</source>
+        <translation>ბაზის ფაილის გახსნა</translation>
+    </message>
+    <message>
+        <source>Output file path</source>
+        <translation>გამოტანის ფაილის ბილიკი</translation>
+    </message>
+    <message>
+        <source>answers.itcl</source>
+        <translation>answers.itcl</translation>
+    </message>
+    <message>
+        <source>iTest off-line client log (*.itcl)</source>
+        <translation>iTest-ის ინტერნეტგარეშე კლიენტის ჟურნალი (*.itcl)</translation>
+    </message>
+    <message>
+        <source>Loading test data...</source>
+        <translation>ტესტის მონაცემების ჩატვირთვა...</translation>
+    </message>
+    <message>
+        <source>iTest version notice</source>
+        <translation>iTest-ის ვერსიის შეტყობინება</translation>
+    </message>
+    <message>
+        <source>You need a newer version of iTest to open this database file.</source>
+        <translation>ამ მონაცემთა ბაზის ფაილის გასახსნელად iTest-ის უფრო ახალი ვერსია გჭირდებათ.</translation>
+    </message>
+    <message>
+        <source>Reading database file...</source>
+        <translation>მიმდინარეობს მონაცემთა ბაზის ფაილის კითხვა...</translation>
+    </message>
+    <message>
+        <source>Open database</source>
+        <translation>მონაცემთა ბაზის გახსნა</translation>
+    </message>
+    <message>
+        <source>Cannot read file %1:
+%2.</source>
+        <translation>ფაილის%1 წაკითხვის შეცდომა:
+%2.</translation>
+    </message>
+    <message>
+        <source>Generating test...</source>
+        <translation>მიმდინარეობს ტესტის გენერაცია...</translation>
+    </message>
+    <message>
+        <source>Finish the exam</source>
+        <translation>გამოცდის დასრულება</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to finish? Once you click Finish there is no going back.</source>
+        <translation>მართლა გნებავთ დასრულება? როცა დასრულებაზე დააწკაპუნებთ, უკან ვეღარ დაბრუნდებით.</translation>
+    </message>
+    <message>
+        <source>&amp;Finish</source>
+        <translation>&amp;დასრულება</translation>
+    </message>
+    <message>
+        <source>&amp;Cancel</source>
+        <translation>&amp;შეწყვეტა</translation>
+    </message>
+    <message>
+        <source>Your name, please.</source>
+        <translation>თქვენი სახელი, გეთაყვა.</translation>
+    </message>
+    <message>
+        <source>You cannot proceed unless you give us your name.</source>
+        <translation>გაგრძელება შეუძლებელია, სანამ თქვენს სახელს არ შეიყვანეთ.</translation>
+    </message>
+    <message>
+        <source>Exam finished</source>
+        <translation>გამოცდა დასრულდა</translation>
+    </message>
+    <message>
+        <source>You have run out of time. Your answers are being sent.</source>
+        <translation>დრო ამოიწურა. მიმდინარეობს თქვენი პასუხების გაგზავნა.</translation>
+    </message>
+    <message>
+        <source>Invalid data received. There might be something wrong with the server.</source>
+        <translation>მიღებული მონაცემები არასწორია. შეიძლება, სერვერს რაღაც სჭირს.</translation>
+    </message>
+    <message>
+        <source>Connect to an iTest server</source>
+        <translation>მიერთება iTest-ი სერვერთან</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Server name&lt;/b&gt;</source>
+        <translation>&lt;b&gt;სერვერის სახელი&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Server port&lt;/b&gt;</source>
+        <translation>&lt;b&gt;სერვერის პორტი&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>Connect</source>
+        <translation>მიერთება</translation>
+    </message>
+    <message>
+        <source>Use an iTest database file</source>
+        <translation>iTest-ის მონაცემთა ბაზი ფაილის გამოყენება</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Database file path&lt;/b&gt;</source>
+        <translation>&lt;b&gt;მონაცემთა ბაზის ფაილის ბილიკი&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>Browse</source>
+        <translation>პოვნა</translation>
+    </message>
+    <message>
+        <source>Load</source>
+        <translation>ჩატვირთვა</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Save answers to&lt;/b&gt;</source>
+        <translation>&lt;b&gt;პასუხების შენახვა&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>If connected to an iTest server, results will be sent automatically. You only need this file if there is no network available or in case of connection breakdown.</source>
+        <translation>თუ მიუერთდებით iTest-ის სერვერს, შედეგები ავტომატურად გაიგზავნება. ეს ფაილი, მხოლოდ, მაშინ გჭირდებათ, თუ ქსელი ხელმიუწვდომელი ან დაზიანებულია.</translation>
+    </message>
+    <message>
+        <source>$HOME/TestName-Date-SurnameName.itcl, where $HOME is the directory of the current user&apos;s profile on Windows and the home directory on Unix/Linux/Mac OS</source>
+        <translation>$HOME/TestName-Date-SurnameName.itcl, სადაც $HOME თქვენი მომხმარებლის პროფილის საქაღალდეა Windows-ზე და საწყისი საქაღალდე Unix/Linux/Mac OS-ზე</translation>
+    </message>
+    <message>
+        <source>Use default</source>
+        <translation>ნაგულისხმების გამოყენება</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Other settings&lt;/b&gt;</source>
+        <translation>&lt;b&gt;სხვა პარამეტრები&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>Hide question names, show numbers instead</source>
+        <translation>კითხვის სახელის დამალვა და მის მაგიერ ნომრების ჩვენება</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Test information&lt;/b&gt;</source>
+        <translation>&lt;b&gt;ტესტის ინფორმაცია&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>Test name</source>
+        <translation>ტესტის სახელი</translation>
+    </message>
+    <message>
+        <source>Database date</source>
+        <translation>მონაცემთა ბაზის თარიღი</translation>
+    </message>
+    <message>
+        <source>Test date</source>
+        <translation>ტესტის თარიღი</translation>
+    </message>
+    <message>
+        <source>Test time</source>
+        <translation>ტესტის დრო</translation>
+    </message>
+    <message>
+        <source>Number of questions</source>
+        <translation>კითხვების რაოდენობა</translation>
+    </message>
+    <message>
+        <source>Number of categories</source>
+        <translation>კატეგორიების რაოდენობა</translation>
+    </message>
+    <message>
+        <source>Category names</source>
+        <translation>კატეგორიის სახელები</translation>
+    </message>
+    <message>
+        <source>Comments</source>
+        <translation>კომენტარები</translation>
+    </message>
+    <message>
+        <source>Data</source>
+        <translation>მონაცემები</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>გასვლა</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>შესახებ</translation>
+    </message>
+    <message>
+        <source>Ready</source>
+        <translation>მზადაა</translation>
+    </message>
+    <message>
+        <source>Your name:</source>
+        <translation>თქვენი სახელი:</translation>
+    </message>
+    <message>
+        <source>When you are ready to begin the examination, click the Start button below.</source>
+        <translation>როცა გამოცდის დასაწყებად მზად იქნებით, დააწკაპუნეთ ქვემოთ დაწყების ღილაკზე.</translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation>დაწყება</translation>
+    </message>
+    <message>
+        <source>Show remaining time</source>
+        <translation>დარჩენილი დროის ჩვენება</translation>
+    </message>
+    <message>
+        <source>minute(s)</source>
+        <translation>წუთი</translation>
+    </message>
+    <message>
+        <source>Last question</source>
+        <translation>ბოლო კითხვა</translation>
+    </message>
+    <message>
+        <source>Next question</source>
+        <translation>შემდეგი კითხვა</translation>
+    </message>
+    <message>
+        <source>Finish</source>
+        <translation>დასრულება</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Results:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;შედეგები:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Score:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;ქულა:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Name:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;სახელი:&lt;/b&gt; %1&lt;br/&gt;</translation>
+    </message>
+    <message>
+        <source>iTest off-line test sessions (*.itos);;All files (*.*)</source>
+        <translation>iTest ინტერნეტგარეშე ტესტის სესიები (*.itos);;ყველა ფაილი (*.*)</translation>
+    </message>
+    <message>
+        <source>%1 out of %2 (%3)</source>
+        <translation>%1 %2-დან (%3)</translation>
+    </message>
+    <message>
+        <source>PASSED</source>
+        <translation>გავლილია</translation>
+    </message>
+    <message>
+        <source>FAILED</source>
+        <translation>წარუმატებელი</translation>
+    </message>
+    <message>
+        <source>Total</source>
+        <translation>სულ</translation>
+    </message>
+    <message>
+        <source>Pass mark</source>
+        <translation>გავლის ნიშანი</translation>
+    </message>
+    <message>
+        <source>New test</source>
+        <translation>ახალი ტესტი</translation>
+    </message>
+    <message>
+        <source>iTestClient</source>
+        <translation>iTestClient</translation>
+    </message>
+    <message>
+        <source>iTestClient - Load test data</source>
+        <translation>iTestClient - ტესტის მონაცემების ჩატვირთვა</translation>
+    </message>
+    <message>
+        <source>%1 of total %2</source>
+        <translation>%1 სულ %2-დან</translation>
+    </message>
+    <message>
+        <source>Do not show correct answers at the end of the test</source>
+        <translation>ტესტის ბოლოს სწორი პასუხები ნაჩვენები არ იქნება</translation>
+    </message>
+    <message>
+        <source>LTR</source>
+        <translation>მარცხნიდან მარჯვნივ</translation>
+    </message>
+    <message>
+        <source>Welcome!</source>
+        <translation>მოგესალმებით!</translation>
+    </message>
+    <message>
+        <source>There is a newer version of iTest available.
+Nonetheless, this version is able to open the database file you selected,
+but you are most probably missing a whole bunch of cool new features.</source>
+        <translation type="obsolete">Доступна более новая версия iTest.
+Выбранный файл базы данных может быть открыт данной версией программы,
+но Вы можете пропустить большое количество новых возможностей.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>a)</source>
+        <translation>ა)</translation>
+    </message>
+    <message>
+        <source>b)</source>
+        <translation>ბ)</translation>
+    </message>
+    <message>
+        <source>c)</source>
+        <translation>გ)</translation>
+    </message>
+    <message>
+        <source>d)</source>
+        <translation>დ)</translation>
+    </message>
+    <message>
+        <source>e)</source>
+        <translation>ე)</translation>
+    </message>
+    <message>
+        <source>f)</source>
+        <translation>ვ)</translation>
+    </message>
+    <message>
+        <source>g)</source>
+        <translation>ზ)</translation>
+    </message>
+    <message>
+        <source>h)</source>
+        <translation>თ</translation>
+    </message>
+    <message>
+        <source>i)</source>
+        <translation>ი)</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>არაფერი</translation>
+    </message>
+</context>
+<context>
+    <name>QuestionWidget</name>
+    <message>
+        <source>%1 out of %2</source>
+        <translation>%1 %2-დან</translation>
+    </message>
+</context>
+</TS>
